37dcb936e0ecc7cff5c3aa26a8b0dbc3065f469d
Hannah <hyoung20@gmail.com>
2013-10-09 15:48:40 -0700

    Initial commit.
    Signed-off-by: Hannah <hyoung20@gmail.com>
    
    	new file:   MovieProject/.classpath
    	new file:   MovieProject/.project
    	new file:   MovieProject/.settings/org.eclipse.jdt.core.prefs
    	new file:   MovieProject/.settings/org.eclipse.wst.common.component
    	new file:   MovieProject/.settings/org.eclipse.wst.common.project.facet.core.xml
    	new file:   MovieProject/.settings/org.eclipse.wst.validation.prefs
    	new file:   MovieProject/README.txt
    	new file:   MovieProject/pom.xml
    	new file:   MovieProject/src/main/java/movieproject/controller/UserController.java
    	new file:   MovieProject/src/main/java/movieproject/dao/UserDao.java
    	new file:   MovieProject/src/main/java/movieproject/entities/User.java
    	new file:   MovieProject/src/main/java/movieproject/helpers/Login.java
    	new file:   MovieProject/src/main/java/movieproject/helpers/RegEx.java
    	new file:   MovieProject/src/main/java/movieproject/helpers/Registration.java
    	new file:   MovieProject/src/main/java/movieproject/response/Response.java
    	new file:   MovieProject/src/main/java/movieproject/utilities/ResponseFactory.java
    	new file:   MovieProject/src/main/java/movieproject/utilities/UserFactory.java
    	new file:   MovieProject/src/main/resources/META-INF/persistence.xml
    	new file:   MovieProject/src/main/resources/log4j.properties
    	new file:   MovieProject/src/main/webapp/WEB-INF/spring-servlet.xml
    	new file:   MovieProject/src/main/webapp/WEB-INF/views/welcome.jsp
    	new file:   MovieProject/src/main/webapp/WEB-INF/web.xml
    	new file:   MovieProject/src/main/webapp/resources/spring_logo.png
    	new file:   MovieProject/target/classes/META-INF/persistence.xml
    	new file:   MovieProject/target/classes/log4j.properties
    	new file:   MovieProject/target/classes/movieproject/controller/UserController.class
    	new file:   MovieProject/target/classes/movieproject/dao/UserDao.class
    	new file:   MovieProject/target/classes/movieproject/entities/User.class
    	new file:   MovieProject/target/classes/movieproject/helpers/Login.class
    	new file:   MovieProject/target/classes/movieproject/helpers/RegEx.class
    	new file:   MovieProject/target/classes/movieproject/helpers/Registration.class
    	new file:   MovieProject/target/classes/movieproject/response/Response.class
    	new file:   MovieProject/target/classes/movieproject/utilities/ResponseFactory.class
    	new file:   MovieProject/target/classes/movieproject/utilities/UserFactory.class
    	new file:   MovieProject/target/m2e-wtp/web-resources/META-INF/MANIFEST.MF
    	new file:   MovieProject/target/m2e-wtp/web-resources/META-INF/maven/com.syreo.movieproject/movieproject/pom.properties
    	new file:   MovieProject/target/m2e-wtp/web-resources/META-INF/maven/com.syreo.movieproject/movieproject/pom.xml
    	new file:   README.md

---
+++ MovieProject/src/main/java/movieproject/dao/UserDao.java
@@ -0,0 +1,116 @@
+package movieproject.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import movieproject.entities.User;
+import movieproject.response.Response;
+import movieproject.utilities.ResponseFactory;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+/**
+ * UserDao Class
+ * @author hyoung
+ *
+ */
+@Component
+public class UserDao {
+
+	private ResponseFactory responseFactory = new ResponseFactory();
+	
+	@PersistenceContext(unitName = "NatureJournalPU")
+	private EntityManager em;
+	
+	/**
+	 * Setting entity manager
+	 */
+	public void setEM(EntityManager em) {
+		this.em = em;
+	}
+	
+	/**
+	 * 
+	 * persists user to the database.
+	 */
+	@Transactional
+	public void persist(User user) {
+		em.persist(user);
+	}
+
+	/**
+	 * 
+	 * updates user in the database
+	 */
+	@Transactional
+	public void update(User user) {
+		em.merge(user);
+	}
+	
+	
+
+	
+	
+	@Transactional
+	public User findByUserName(String userName) {
+		/*
+		 * queries DB for user matching query params
+		 */
+		Query query = em.createNamedQuery("findByUserName");
+		query.setParameter("userName", userName);
+		User temp = new User();
+		try {
+			temp = (User) query.getSingleResult();
+
+		} catch (Exception e) {
+			return null;
+		}
+		return temp;
+	}
+	
+	/**
+	 * find users by their user ids. 
+	 * @param userId
+	 * @return
+	 */
+	@Transactional
+	public User findUserByUserId(int userId) {
+		User temp = null;
+
+		Query query = em.createNamedQuery("findByUserId");
+		query.setParameter("userId", userId);
+
+		try {
+			temp = (User) query.getSingleResult();
+
+		} catch (RuntimeException e) {
+
+		}
+		return temp;
+
+	}
+	
+	/**
+	 * Gets a list of all users in the database
+	 */
+	@SuppressWarnings("unchecked")
+	@Transactional
+	public List<User> getAllUsers() {
+
+		Query query = em.createNamedQuery("findAllUsers");
+		return query.getResultList();
+
+	}
+	
+	@Transactional
+	public String getUserNameByUserId(int userId){
+		Query query = em.createNamedQuery("");
+		query.setParameter("userId", userId);
+		
+		return (String) query.getSingleResult();
+	}
+}
